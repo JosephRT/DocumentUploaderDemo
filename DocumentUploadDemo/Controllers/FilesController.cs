d2a834e03feda96456e54d2c752ceac007b37468
root <root@JOES-DESKTOP.localdomain>
2020-08-06 17:29:45 -0400

    Cleaning out some of the initial placeholder code.  Adding some of the (really ugly) file upload streaming reference code from MSDN.  Actually worked with a bit of fiddling.
    Onto lots of refactoring.

---
+++ DocumentUploadDemo/Controllers/FilesController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using DocumentUploadDemo.Utilities;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+
+namespace DocumentUploadDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly long fileSizeLimit = 1000;
+        private readonly string[] permittedExtensions = {".txt"};
+        private readonly string targetFilePath = "c:\\files";
+
+        // Get the default form options so that we can use them to set the default 
+        // limits for request body data.
+        private static readonly FormOptions DefaultFormOptions = new FormOptions();
+
+        // GET: api/<FilesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] {"value1", "value2"};
+        }
+
+        // GET api/<FilesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<FilesController>
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                ModelState.AddModelError("File",
+                    "The request couldn't be processed (Error 1).");
+                // Log error
+
+                return BadRequest(ModelState);
+            }
+
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                DefaultFormOptions.MultipartBoundaryLengthLimit);
+            var reader = new MultipartReader(boundary, HttpContext.Request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader =
+                    ContentDispositionHeaderValue.TryParse(
+                        section.ContentDisposition, out var contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    // This check assumes that there's a file
+                    // present without form data. If form data
+                    // is present, this method immediately fails
+                    // and returns the model error.
+                    if (!MultipartRequestHelper
+                        .HasFileContentDisposition(contentDisposition))
+                    {
+                        ModelState.AddModelError("File",
+                            "The request couldn't be processed (Error 2).");
+                        // Log error
+
+                        return BadRequest(ModelState);
+                    }
+
+                    // Don't trust the file name sent by the client. To display
+                    // the file name, HTML-encode the value.
+                    var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                        contentDisposition.FileName.Value);
+                    var trustedFileNameForFileStorage = Path.GetRandomFileName();
+
+                    // **WARNING!**
+                    // In the following example, the file is saved without
+                    // scanning the file's contents. In most production
+                    // scenarios, an anti-virus/anti-malware scanner API
+                    // is used on the file before making the file available
+                    // for download or for use by other systems. 
+                    // For more information, see the topic that accompanies 
+                    // this sample.
+
+                    var streamedFileContent = await FileHelpers.ProcessStreamedFile(
+                        section, contentDisposition, ModelState,
+                        permittedExtensions, fileSizeLimit);
+
+                    if (!ModelState.IsValid) return BadRequest(ModelState);
+
+                    await using var targetStream = System.IO.File.Create(Path.Combine(targetFilePath, trustedFileNameForFileStorage));
+                    await targetStream.WriteAsync(streamedFileContent);
+
+                    //_logger.LogInformation(
+                    //    "Uploaded file '{TrustedFileNameForDisplay}' saved to " +
+                    //    "'{TargetFilePath}' as {TrustedFileNameForFileStorage}",
+                    //    trustedFileNameForDisplay, targetFilePath,
+                    //    trustedFileNameForFileStorage);
+                }
+
+                // Drain any remaining section body that hasn't been consumed and
+                // read the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            return Created(nameof(FilesController), null);
+        }
+
+        // PUT api/<FilesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<FilesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
