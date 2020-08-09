4aa15e2cba11c6058a163be9edcf4a6cf10f3634
Joe Thompson <josephrt@gmail.com>
2020-08-09 12:32:13 -0400

    Adding more tests, tweaking FileUploadRequest due to tests

---
+++ DocumentUploadApiTests/FileUploadRequestFactoryTests.cs
@@ -0,0 +1,35 @@
+﻿using DocumentUploadDemo.Utilities;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NUnit.Framework;
+
+namespace DocumentUploadApiTests
+{
+    [TestFixture]
+    public class FileUploadRequestFactoryTests
+    {
+        [Test]
+        public void RequestWithMultipartContentCreatesStreamingRequest()
+        {
+            var mockRequest = new Mock<HttpRequest>();
+            mockRequest.SetupGet(r => r.ContentType).Returns("multipart/form-data");
+
+            var factory = new FileUploadRequestFactory();
+            var createdRequest = factory.GetFileUploadRequest(mockRequest.Object);
+
+            Assert.That(createdRequest, Is.TypeOf<StreamingFileUploadRequest>());
+        }
+
+        [Test]
+        public void RequestWithoutMultipartContentCreatesPostBodyRequest()
+        {
+            var mockRequest = new Mock<HttpRequest>();
+            mockRequest.SetupGet(r => r.ContentType).Returns("text/plain");
+
+            var factory = new FileUploadRequestFactory();
+            var createdRequest = factory.GetFileUploadRequest(mockRequest.Object);
+
+            Assert.That(createdRequest, Is.TypeOf<PostBodyFileUploadRequest>());
+        }
+    }
+}
---
+++ DocumentUploadApiTests/FileUploadRequestTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentUploadDemo.Utilities;
+using NUnit.Framework;
+
+namespace DocumentUploadApiTests
+{
+    [TestFixture]
+    public class FileUploadRequestTests
+    {
+        [Test]
+        public async Task ValidStreamIsReadIntoByteArray()
+        {
+            var testStream = new MemoryStream(Encoding.UTF8.GetBytes("TestStreamContents"));
+
+            var result = await FileUploadRequest.ProcessStreamContents(testStream);
+
+            Assert.That(result, Is.EqualTo(testStream.ToArray()));
+        }
+
+        [Test]
+        public void EmptyStreamReturnsException()
+        {
+            var testStream = new MemoryStream();
+
+            Assert.That(async () => await FileUploadRequest.ProcessStreamContents(testStream), Throws.TypeOf<InvalidFileUploadException>()
+                .And.Message.EqualTo("The file is empty"));
+        }
+
+        [Test]
+        public void NullStreamReturnsException()
+        {
+            Assert.That(async () => await FileUploadRequest.ProcessStreamContents(null), Throws.TypeOf<Exception>()
+                .And.Message.EqualTo("File upload failed"));
+        }
+    }
+}
